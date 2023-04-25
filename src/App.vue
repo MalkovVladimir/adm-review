88c4db9e3380fca746d7d3d89072c0f231d95c52
Vladimir Malkov <vladimir.malkov@admiralmarkets.com>
2023-04-25 13:48:47 +0300

    feat: Implement todo

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Todo />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Todo from './components/todo.vue';
+
+export default defineComponent({
+  components: {
+    Todo
+  }
+});
+</script>
---
+++ src/components/todo.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="todo">
+    <div class="todo__header">
+      <input ref="searchInput" type="text" class="todo-header__input">
+      <button @click="addNewTodo">Add</button>
+    </div>
+    <div class="todo__body">
+      <div v-for="value in items">
+        <Item :value="value" @remove="remove" />
+      </div>
+    </div>
+    <button class="todo__clear" @click="clear">Clear all</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, Ref, ref } from 'vue';
+import Item from './Item.vue';
+import todoDefaultItems from '../default-items/todoDefaultItems'
+
+export default defineComponent({
+  setup() {
+    var items: Ref<any[]> = ref(todoDefaultItems);
+    const searchInput = ref<HTMLInputElement | null>(null);
+    const addNewTodo = () => {
+      items.value.push({
+        id: items.value.length,
+        value: searchInput.value?.value,
+        is_done: false
+      })
+    }
+    return {
+      searchInput,
+      items,
+      addNewTodo
+    };
+  },
+  methods: {
+    clear() {
+      this.items = [];
+    },
+    remove(id: number) {
+      delete this.items[id];
+    }
+  },
+  components: { Item }
+});
+</script>
+
+<style scoped>
+.todo {
+  display: grid;
+  grid-template-rows: min-content 1fr min-content;
+  height: 500px;
+  width: 300px;
+  padding: 1rem;
+  border: 1px solid lightcyan;
+  border-radius: 1rem;
+}
+
+.todo__header {
+  display: flex;
+  gap: 1rem;
+}
+
+.todo-header__input {
+  width: 100%;
+}
+
+.todo__body {
+  display: grid;
+  grid-auto-rows: min-content;
+  gap: 0.3rem;
+  text-align: left;
+  margin: 1rem 0;
+}
+
+.todo__clear:hover {
+  background-color: crimson;
+}
+</style>
